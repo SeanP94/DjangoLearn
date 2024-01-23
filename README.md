e69884251b44e29a3c66d387f0881dda285ab771
seanperry <seanep94@gmail.com>
2024-01-22 21:18:55 -0800

    End of the Django tutorial free of Very Academy - Will be moving onto his actualy Django/Htmx course

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ make an venv called djangoenv*
 
 django-admin startprojectcore <dir>
 
+you can use python3 manage.py shell to access a shell where you can look at stuff like newapp.models :) 
--- newapp/views.py
+++ newapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import CustomerForm
 # Create your views here.
+
+def addnew(request):
+    if request.method == 'POST':
+        formset = CustomerForm(request.POST)
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect("/")
+    else:
+        formset = CustomerForm()
+        return render(request, 'addnew.html', {'formset' : formset})
+
 
 def home(request):
     return render(request, 'index.html')
